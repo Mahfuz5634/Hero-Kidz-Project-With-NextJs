18ffa2d8c07ca717eb9d6c489d5d96227ff72a8e
Mahfuz <mahfuzru5634@gmail.com>
2025-12-24 00:24:39 +0600

    do all initial setup

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="bg-green-400 text-6xl">hello nextjs</h1>
+      <p className="text-black">hdhdfh dhfdof dfdofhdof </p>
+    </div>
+  );
+}
